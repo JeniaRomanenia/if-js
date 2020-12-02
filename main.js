59a4a81ad37e012669bdc1e710a46e54c5387da9
JeniaRomanenia <jenia02041985@gmail.com>
2020-12-02 20:43:57 +0300

    Click event Sum function

--- main.js
+++ main.js
@@ -36,3 +36,33 @@ for (let i = 0; i <array2.length; i++) {
         console.log(array2[i]);
     }
 }
+
+// функция sum
+function sum(a = 0) {
+    return function(b = 0) {
+        return a + b;
+    };
+}
+console.log(sum(5)(2));
+
+// Покрасьте абзацы по клику (событие click)
+const text1El = document.getElementById('text1');
+const text2El = document.getElementById('text2');
+const text3El = document.getElementById('text3');
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+text1El.addEventListener('click', colorText());
+text2El.addEventListener('click', colorText());
+text3El.addEventListener('click', colorText());
+
+function colorText (){
+    let j = 0;
+    return function (){
+        this.style.color = colors[j];
+        j++;
+        if (j == colors.length){
+            j = 0;
+        }
+    }
+}
