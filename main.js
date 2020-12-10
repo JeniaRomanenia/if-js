09e750bd64f0597e449855936238160e0a8f3b89
JeniaRomanenia <jenia02041985@gmail.com>
2020-12-10 22:12:34 +0300

    Date format conversion Search for accommodation facilities

--- main.js
+++ main.js
@@ -36,3 +36,165 @@ for (let i = 0; i <array2.length; i++) {
         console.log(array2[i]);
     }
 }
+
+//функция palindrome
+function Palindrome(word) {
+    let Reverse = word.split('').reverse().join('');
+    if (Reverse === word) {
+        return true;
+    }
+        return false;
+}
+console.log(Palindrome('шалаш'));
+
+//функция min(a, b),возвращает меньшее из чисел
+function min(a, b) {
+    if (a < b) {
+        return a;
+    }
+        return b;
+}
+console.log(min(8, 3)); // 3
+
+//функция max(a, b),возвращает большее из чисел
+function max(a, y) {
+    if (a > y) {
+        return a;
+    }
+        return y;
+}
+console.log(max(4, 2)); // 4
+
+//функция min(a, b) с тернарным оператором
+function min1(a, b) {
+    return a < b ? a : b;
+}
+console.log(min1(4, 3)); // 3
+
+//функция max(a, b) с тернарным оператором
+function max1(a, y) {
+    return a > y ? a : y;
+}
+console.log(max1(2, 8)); // 8
+
+//функция, которая заменяет все 0 на 'zero'
+let array3 = [ ];
+for (let i = 0; i < 10; i++) {
+    array3[i] = Math.floor(Math.random() * 101);
+}
+console.log(array3);
+
+function myFunc(array) {
+    for (let i = 0; i < array.length; i++) {
+        let b = String(array[i]);
+        if (b.endsWith('0')) {
+            b = b.replaceAll('0', 'zero');
+            array[i] = b;
+        }
+    }
+    return array;
+}
+
+console.log(myFunc(array3));
+
+// функция sum
+function sum(a = 0) {
+    return function(b = 0) {
+        return a + b;
+    };
+}
+console.log(sum(5)(2));
+
+// Покрасьте абзацы по клику (событие click)
+const text1El = document.getElementById('text1');
+const text2El = document.getElementById('text2');
+const text3El = document.getElementById('text3');
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+const colorText = () => {
+    let j = 0;
+    return (event) => {
+        event.target.style.color = colors[j];
+        j = j > 3 ? 0 : j + 1;
+        };
+    };
+
+text1El.addEventListener('click', colorText());
+text2El.addEventListener('click', colorText());
+text3El.addEventListener('click', colorText());
+
+//преобразование формата даты: '2020-11-26' => '26.11.2020'
+const date = '2020-11-26';
+const dateFormat = (date2) => date2.split('-').reverse().join('.');
+
+console.log(dateFormat(date));
+
+//поиск объектов размещения:
+const data = [
+    {
+        country: 'Russia',
+        city: 'Saint Petersburg',
+        hotel: 'Hotel Leopold',
+    },
+    {
+        country: 'Spain',
+        city: 'Santa Cruz de Tenerife',
+        hotel: 'Apartment Sunshine',
+    },
+    {
+        country: 'Slowakia',
+        city: 'Vysokie Tatry',
+        hotel: 'Villa Kunerad',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hostel Friendship',
+    },
+    {
+        country: 'Indonesia',
+        city: 'Bali',
+        hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+        country: 'Netherlands',
+        city: 'Rotterdam',
+        hotel: 'King Kong Hostel',
+    },
+    {
+        country: 'Marocco',
+        city: 'Ourika',
+        hotel: 'Rokoko Hotel',
+    },
+    {
+        country: 'Germany',
+        city: 'Berlin',
+        hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+];
+
+const replaceStrForSearch = str => str.toLowerCase().replaceAll(' ', '');
+
+const search = str => {
+    const searchStr = replaceStrForSearch(str);
+
+    return data
+        .filter(object => (replaceStrForSearch(object.country.toLowerCase().replaceAll(' ', '')).includes(searchStr)
+        ||replaceStrForSearch(object.city.toLowerCase().replaceAll(' ', '')).includes(searchStr)
+            ||replaceStrForSearch(object.hotel.toLowerCase().replaceAll(' ', '')).includes(searchStr))
+        ).map(object => `Страна: ${object.country} Город: ${object.city} Отель: ${object.hotel}`)
+};
+
+console.log(search('berlin'));
+
+
+
+
+
+
+
+
+
+
+
